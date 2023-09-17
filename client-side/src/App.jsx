7379f1998ed8651d520344c4c2e5a911956a99b8
rohan5901 <rohangarg5901@gmail.com>
2023-09-17 21:22:35 +0530

    first commit

---
+++ client-side/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../front-end/src/Components/Navbar"
+import Home from "../../front-end/src/Components/Home"
+
+function App() {
+  return (
+    <div className="relative">
+      <Navbar></Navbar>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className='text-red'>
+        <img src="./snacks.jpg" alt="img"/>
+        <div className="absolute">
+            <p className="text-red">Rohan</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
