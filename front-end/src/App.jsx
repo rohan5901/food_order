6903a7db2634aa424be7165238ee0f323cb39fe4
Akshat-Garg7 <akshatgarg280503@gmail.com>
2023-09-18 14:55:19 +0530

    added the parallax effect and optimizing the navbar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 
 function App() {
   return (
-    <div className='relative'>
-    <Navbar></Navbar>
+    <div>
     <Home></Home>
+    
+    <div className="h-100 bg-red-400 text-5xl absolute">
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+    
     
     </div>
   )
